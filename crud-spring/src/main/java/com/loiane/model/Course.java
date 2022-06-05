cc49ce6cabbeff296f2d6c1a79bf5626f7067114
Loiane <loianeg@gmail.com>
2022-06-05 13:58:57 -0400

    ✨feature: added Bean validation to model

--- crud-spring/src/main/java/com/loiane/model/Course.java
+++ crud-spring/src/main/java/com/loiane/model/Course.java
@@ -1,5 +1,7 @@
 package com.loiane.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,12 +9,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Entity
 public class Course {
@@ -22,9 +29,15 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 200)
     @Column(length = 200, nullable = false)
     private String name;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "back-end|front-end")
     @Column(length = 10, nullable = false)
     private String category;
 }
