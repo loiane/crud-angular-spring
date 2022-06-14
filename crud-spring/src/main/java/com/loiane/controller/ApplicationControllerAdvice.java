155746d0ec885dab723c7ac2f17bdefd1f76a4f4
Loiane <loianeg@gmail.com>
2022-06-14 10:48:24 -0400

    ✨feature: added controller exception advice

---
+++ crud-spring/src/main/java/com/loiane/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,39 @@
+package com.loiane.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.loiane.exception.RecordNotFoundException;
+
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundException(RecordNotFoundException e) {
+        return e.getMessage();
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public FieldError[] validationError(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        final List<FieldError> fieldErrors = result.getFieldErrors();
+        return fieldErrors.toArray(new FieldError[fieldErrors.size()]);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleConstraintViolationException(ConstraintViolationException e) {
+        return "not valid due to validation error: " + e.getMessage();
+    }
+}
