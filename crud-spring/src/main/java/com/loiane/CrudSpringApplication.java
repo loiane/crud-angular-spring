76d19ac798e6ccf22af1c9d4ace9ecb26567553f
Loiane <loianeg@gmail.com>
2022-12-06 12:21:27 -0500

    added dev profile

--- crud-spring/src/main/java/com/loiane/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/loiane/CrudSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import com.loiane.enums.Category;
 import com.loiane.enums.Status;
@@ -17,20 +18,21 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
-	// @Bean
-	// CommandLineRunner initDatabase(CourseRepository courseRepository) {
-	// return args -> extracted(courseRepository);
-	// }
+	@Bean
+	@Profile("dev")
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> extracted(courseRepository);
+	}
 
-	// private void extracted(CourseRepository courseRepository) {
-	// courseRepository.deleteAll();
-	// for (int i = 1; i < 20; i++) {
-	// Course c = new Course();
-	// c.setName("Curso " + i);
-	// c.setCategory(Category.FRONT_END);
-	// c.setStatus(Status.ACTIVE);
-	// courseRepository.save(c);
-	// }
-	// }
+	private void extracted(CourseRepository courseRepository) {
+		courseRepository.deleteAll();
+		for (int i = 1; i < 20; i++) {
+			Course c = new Course();
+			c.setName("Curso " + i);
+			c.setCategory(Category.FRONT_END);
+			c.setStatus(Status.ACTIVE);
+			courseRepository.save(c);
+		}
+	}
 
 }
