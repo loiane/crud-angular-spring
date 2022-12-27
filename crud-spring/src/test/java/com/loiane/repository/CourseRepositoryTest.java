d5273113acea3eb493a768111e8dbc300761523c
Loiane <loianeg@gmail.com>
2022-12-27 10:57:43 -0500

    sonar lint fix: Unnecessary `@SpringExtension`

--- crud-spring/src/test/java/com/loiane/repository/CourseRepositoryTest.java
+++ crud-spring/src/test/java/com/loiane/repository/CourseRepositoryTest.java
@@ -14,14 +14,12 @@ import jakarta.validation.ConstraintViolationException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -31,7 +29,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * In practice, only additional methods to the interface should be tested.
  */
 @ActiveProfiles("test")
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class CourseRepositoryTest {
 
