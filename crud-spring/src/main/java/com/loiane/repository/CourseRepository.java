a744fee90031f5cc35b6ffc8556a726c67ad7b8a
Loiane <loianeg@gmail.com>
2022-12-27 10:57:13 -0500

    sonar lint fix: Unnecessary `@Repository`

--- crud-spring/src/main/java/com/loiane/repository/CourseRepository.java
+++ crud-spring/src/main/java/com/loiane/repository/CourseRepository.java
@@ -6,12 +6,10 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.loiane.enums.Status;
 import com.loiane.model.Course;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     Page<Course> findByStatus(Pageable pageable, Status status);
