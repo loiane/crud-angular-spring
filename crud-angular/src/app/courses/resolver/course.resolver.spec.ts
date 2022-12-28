4ea5b27a0e200335b616d92247080727754f24f1
Loiane <loianeg@gmail.com>
2022-12-28 16:52:33 -0500

    added tests for CourseResolver

--- crud-angular/src/app/courses/resolver/course.resolver.spec.ts
+++ crud-angular/src/app/courses/resolver/course.resolver.spec.ts
@@ -1,14 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { CoursesService } from '../services/courses.service';
+import { of } from 'rxjs';
 
+import { CoursesService } from '../services/courses.service';
 import { CourseResolver } from './course.resolver';
 
 describe('CourseResolver', () => {
   let resolver: CourseResolver;
-  let courseServiceMock: any;
+  let courseServiceSpy: jasmine.SpyObj<CoursesService>;
 
   beforeEach(() => {
-    const courseServiceSpy = jasmine.createSpyObj('CoursesService', ['loadById']);
+    courseServiceSpy = jasmine.createSpyObj('CoursesService', ['loadById']);
     TestBed.configureTestingModule({
       providers: [{ provide: CoursesService, useValue: courseServiceSpy }]
     });
@@ -17,5 +18,30 @@ describe('CourseResolver', () => {
 
   it('should be created', () => {
     expect(resolver).toBeTruthy();
+  });
+
+  it('should call loadById with id', () => {
+    const courseService = TestBed.inject(CoursesService);
+    resolver.resolve({ params: { id: 1 } } as any, {} as any);
+    expect(courseService.loadById).toHaveBeenCalledWith('1');
+  });
+
+  it('should return course', () => {
+    const course = {
+      _id: '1',
+      name: 'Angular',
+      category: 'Angular description',
+      lessons: []
+    };
+    courseServiceSpy.loadById.and.returnValue(of(course));
+    const result = resolver.resolve({ params: { id: 1 } } as any, {} as any);
+    expect(result).toEqual(of(course));
+  });
+
+  it('should return empty course if new', () => {
+    const course = { _id: '', name: '', category: '', lessons: [] };
+    courseServiceSpy.loadById.and.returnValue(of(course));
+    const result = resolver.resolve({ params: {} } as any, {} as any);
+    expect(result).toEqual(of(course));
   });
 });
