0d805c36e9b2c3b73aa5baceb788935807d5eb04
Loiane <loianeg@gmail.com>
2025-07-07 21:03:48 -0400

    chore(dependencies): update Spring Boot version to 3.5.3 chore: remove unused import in Lesson.java

--- crud-spring/src/main/java/com/loiane/course/Lesson.java
+++ crud-spring/src/main/java/com/loiane/course/Lesson.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OrderBy;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
