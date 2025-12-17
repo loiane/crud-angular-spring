0b2104d5fb9ba8bbaa243c193ebc4dba13fafd4a
Loiane <loianeg@gmail.com>
2025-12-17 18:27:01 -0500

    refactor(validation): remove nullable annotation from method parameter
    - Removed the @Nullable annotation from the 'target' parameter in the before method.
    - Simplified the method signature for clarity.

--- crud-spring/src/test/java/com/loiane/config/ValidationAdvice.java
+++ crud-spring/src/test/java/com/loiane/config/ValidationAdvice.java
@@ -9,7 +9,6 @@ import jakarta.validation.executable.ExecutableValidator;
 
 import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.context.ApplicationContext;
-import org.springframework.lang.Nullable;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.SpringConstraintValidatorFactory;
 
@@ -27,9 +26,8 @@ public class ValidationAdvice implements MethodBeforeAdvice {
         executableValidator = factory.getValidator().forExecutables();
     }
 
-    @SuppressWarnings("null")
     @Override
-    public void before(Method method, Object[] args, @Nullable Object target) {
+    public void before(Method method, Object[] args, Object target) {
         Set<ConstraintViolation<Object>> violations = executableValidator.validateParameters(target, method, args);
         if (!violations.isEmpty()) {
             throw new ConstraintViolationException(violations);
